03b7f7647d8fd54d533d2a8ba8942df669c09f15
XYH <xyh1020642083@outlook.com>
2024-04-24 18:33:13 +0800

    修复鼠标提前放到相应的下拉菜单处，在滚轮上移且正好达到页面最顶部出现导航栏文字消失的bug

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,21 @@ let head_text = document.querySelectorAll('.language a,.language span,.wing_news
 let head_nav = document.querySelectorAll('.wing_news_nav_m>li');
 let head_bg = document.querySelector('.wing_news_nav_bg');
 let head_flag = 1; //head_flag用于判断是否满足二级菜单唤出的条件，不然鼠标放在导航栏滚动向下滚动会因为visibility不释放位置导致反复唤出二级菜单
+// 实时获取鼠标位置
+let mouseY = 0;
+// 该监听事件即使物理鼠标没有移动，但页面滚动依然能监听
+//用于修复鼠标提前放到相应的下拉菜单处，在滚轮上移且正好达到页面最顶部出现导航栏文字消失的bug
+document.addEventListener('mousemove', function (e) {
+    mouseY = e.clientY;
+    if (window.pageYOffset == 0 && mouseY > 72) {//置顶清空所有样式
+        head_bg.classList.remove('wing_nav_move');
+        head_logo.src = 'images/tx_logo.png';
+        for (let j = 0; j < head_text.length; j++) {
+            head_text[j].style.color = '#FFF';
+        }
+        head_flag = 1;
+    }
+})
 for (let i = 0; i < head_nav.length; i++) {
     head_nav[i].addEventListener('mouseover', function () {
         if (head_flag == 1) {
@@ -48,7 +63,7 @@ window.addEventListener('scroll', function () {
         }
         head_flag = 1;
     }
-    if (window.pageYOffset == 0) {//置顶清空所有样式
+    if (window.pageYOffset == 0 && mouseY > 72) {//置顶清空所有样式
         head_bg.classList.remove('wing_nav_move');
         head_logo.src = 'images/tx_logo.png';
         for (let j = 0; j < head_text.length; j++) {
